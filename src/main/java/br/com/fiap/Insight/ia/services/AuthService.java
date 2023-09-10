3dfda19e73ad1770985a3e7c669e5d75aa35b340
Gustavo Balero <gubalero@hotmail.com>
2023-09-10 20:46:25 -0300

    Removendo arquivo de teste

--- src/main/java/br/com/fiap/Insight/ia/services/AuthService.java
+++ src/main/java/br/com/fiap/Insight/ia/services/AuthService.java
@@ -3,21 +3,28 @@ package br.com.fiap.Insight.ia.services;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
 import br.com.fiap.Insight.ia.exception.RestNotAuthorizedException;
 import br.com.fiap.Insight.ia.models.Status;
 import br.com.fiap.Insight.ia.models.Usuario;
 import br.com.fiap.Insight.ia.repository.UsuarioRepository;
 
 public class AuthService {
-    
-    public Usuario getUsuarioLogado(UsuarioRepository usuarioRepository){
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getName();
-        
+    public Usuario getUsuarioLogado(UsuarioRepository usuarioRepository) {
+        String email = "";
+
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            email = authentication.getName();
+        } catch (JWTVerificationException e) {
+            throw new RestNotAuthorizedException("Token inválido");
+        }
+
         return usuarioRepository
-            .findByEmail(email)
-            .filter(usuario -> usuario.getStatus().equals(Status.ATIVO))
-            .orElseThrow(() -> new RestNotAuthorizedException("Usuario não encontrada"));
+                .findByEmail(email)
+                .filter(usuario -> usuario.getStatus().equals(Status.ATIVO))
+                .orElseThrow(() -> new RestNotAuthorizedException("Usuario não encontrada"));
     }
 }
